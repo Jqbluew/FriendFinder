ccb896a7021dcd182b4dbc77b4cf9d04a58314a2
JackieP <jqbluew@mac.com>
2018-05-01 12:26:27 -0400

    finished files

---
+++ app/data/friends.js
@@ -0,0 +1,49 @@
+var friends = [
+	
+	{
+		name: "Oprah",
+		photo: "https://www.google.com/search?q=oprah&rlz=1C5CHFA_enUS778US778&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjwm43P3OTaAhWjrVkKHYgvBXQQ_AUICygC&biw=1721&bih=1224#imgrc=c5CiP9yPeQJ1IM:",
+		scores: [
+			"1",
+			"1",
+			"1",
+			"1",
+			"1"
+		]
+	}, 
+		{
+		name: "Mary Barra",
+		photo: "https://www.google.com/search?q=oprah&rlz=1C5CHFA_enUS778US778&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjwm43P3OTaAhWjrVkKHYgvBXQQ_AUICygC&biw=1721&bih=1224#imgrc=c5CiP9yPeQJ1IM:",
+		scores: [
+			"2",
+			"2",
+			"3",
+			"3",
+			"3"
+		]
+	}, 
+		{
+		name: "Anna Wintour",
+		photo: "https://www.google.com/search?q=oprah&rlz=1C5CHFA_enUS778US778&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjwm43P3OTaAhWjrVkKHYgvBXQQ_AUICygC&biw=1721&bih=1224#imgrc=c5CiP9yPeQJ1IM:",
+		scores: [
+			"4",
+			"4",
+			"4",
+			"4",
+			"4"
+		]
+	}, 	
+		{
+		name: "Beyonce",
+		photo: "https://www.google.com/search?q=oprah&rlz=1C5CHFA_enUS778US778&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjwm43P3OTaAhWjrVkKHYgvBXQQ_AUICygC&biw=1721&bih=1224#imgrc=c5CiP9yPeQJ1IM:",
+		scores: [
+			"5",
+			"5",
+			"5",
+			"5",
+			"5"
+		]	
+	}
+];
+
+module.exports = friends;
